602e8c9843e4930a8da8fddd519cb410418241e4
joshmaglione <joshmaglione@gmail.com>
2019-01-25 03:34:44 -0500

    Install & Update files

---
+++ install.sh
@@ -0,0 +1,54 @@
+# Vars
+FILE="$(realpath -s "$0")"              # Full path to this file
+DIR="$(dirname $FILE)"                  # Current directory
+PKGDIR="$(dirname $DIR)"                # Directory for dependencies
+START="${HOME}/.magmarc"                # Magma start file location
+
+# Dependencies and .spec locations
+ATTACH="AttachSpec(\"$DIR/CSS.spec\");"
+ATTACH2="AttachSpec(\"$PKGDIR/TensorSpace/TensorSpace.spec\");"
+
+
+echo "CSS.spec is in $DIR"
+
+
+echo "Dependencies will be downloaded to $PKGDIR"
+
+
+# TensorSpace install/ update
+if [ -f "$PKGDIR/TensorSpace/update.sh" ]
+then
+    echo "Dependencies already installed, updating..."
+    sh "$PKGDIR/TensorSpace/update.sh"
+else
+    echo "Could not find TensorSpace, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/TensorSpace
+fi
+
+echo "Dependencies downloaded."
+
+
+# Construct Magma start file
+
+if [ -f "$START" ]
+then
+    echo "Found a Magma start file"
+    for A in "$ATTACH" "$ATTACH2" do
+        if grep -Fxq "$A" "$START"
+        then
+            echo "Already installed"
+        else
+            echo "$A" >> "$START"
+            echo "Successfully installed"
+        fi
+    done
+else
+    echo "Creating a Magma start file: $START"
+    echo "// Created by an install file for Magma start up." > "$START"
+    for A in "$ATTACH" "$ATTACH2" do
+        echo "$A" >> "$START"
+    done
+    echo "Successfully installed"
+fi
+
---
+++ update.sh
@@ -0,0 +1,30 @@
+# Maybe we can make this git independent or safer?
+
+# Vars
+FILE="$(realpath -s "$0")"
+DIR="$(dirname $FILE)"
+PKGDIR="$(dirname $DIR)"
+
+
+cd "$DIR" 
+
+# Update main package
+echo "Updating CSS package."
+git -q pull origin master 
+
+echo "Now updating dependencies."
+
+# TensorSpace update
+if [ -f "$PKGDIR/TensorSpace/update.sh" ]
+then
+    sh "$PKGDIR/TensorSpace/update.sh"
+else
+    echo "Could not find TensorSpace, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/TensorSpace
+    echo "Installing TensorSpace..."
+    sh "$PKGDIR/TensorSpace/install.sh"
+fi
+
+echo "Successfully updated!"
+
